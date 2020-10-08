cd604acb199833be4e045973bdea603105f142d5
qisashasanudin <qisas.tazkia@ui.ac.id>
2020-10-09 00:58:27 +0700

    v0.9

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:kurir_kopi/models/user.dart';
 import 'package:kurir_kopi/screens/authenticate/authenticate.dart';
 import 'package:kurir_kopi/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     //return either Home or Authenticate widget
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
