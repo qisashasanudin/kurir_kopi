b87846a6a85d0731ced751e1cb55fd653d4ac11e
qisashasanudin <qisas.tazkia@ui.ac.id>
2020-10-09 10:21:32 +0700

    v0.15.1

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kurir_kopi/services/auth.dart';
 import 'package:kurir_kopi/shared/constants.dart';
+import 'package:kurir_kopi/shared/loading.dart';
 
 class Register extends StatefulWidget {
   final Function toggleView;
@@ -13,6 +14,7 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   //text field state
   String name = '';
@@ -22,89 +24,99 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Daftar ke KurirKopi'),
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () {
-                widget.toggleView();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Masuk'))
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (val) => val.isEmpty ? 'Masukkan email' : null,
-                    onChanged: (val) {
-                      setState(() => email = val);
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              elevation: 0.0,
+              title: Text('Daftar ke KurirKopi'),
+              actions: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      widget.toggleView();
                     },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: 'Password'),
-                    obscureText: true,
-                    validator: (val) => val.length < 6
-                        ? 'Masukkan password (6 huruf atau lebih)'
-                        : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                        hintText: 'Konfirmasi password'),
-                    obscureText: true,
-                    validator: (val) =>
-                        val != password ? 'Kedua password harus sama' : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  RaisedButton(
-                    color: Colors.brown[400],
-                    child: Text(
-                      'Daftar',
-                      style: TextStyle(color: Colors.white),
+                    icon: Icon(Icons.person),
+                    label: Text('Masuk'))
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration:
+                              textInputDecoration.copyWith(hintText: 'Email'),
+                          validator: (val) =>
+                              val.isEmpty ? 'Masukkan email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Password'),
+                          obscureText: true,
+                          validator: (val) => val.length < 6
+                              ? 'Masukkan password (6 huruf atau lebih)'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Konfirmasi password'),
+                          obscureText: true,
+                          validator: (val) => val != password
+                              ? 'Kedua password harus sama'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        RaisedButton(
+                          color: Colors.brown[400],
+                          child: Text(
+                            'Daftar',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState.validate()) {
+                              setState(() {
+                                loading = true;
+                              });
+                              dynamic result = await _auth.registerWithEmail(
+                                  email, password);
+                              if (result == null) {
+                                setState(() {
+                                  error = 'Registrasi tidak berhasil.';
+                                  loading = false;
+                                });
+                              }
+                            }
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        ),
+                      ],
                     ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        dynamic result =
-                            await _auth.registerWithEmail(email, password);
-                        if (result == null) {
-                          setState(
-                              () => error = 'Email tidak dapat digunakan.');
-                        }
-                      }
-                    },
                   ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red, fontSize: 14.0),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
