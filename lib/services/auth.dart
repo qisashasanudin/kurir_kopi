0e30b69ba56bb2fd84ccf8584365fd27fa67805c
qisashasanudin <qisas.tazkia@ui.ac.id>
2020-10-09 11:13:44 +0700

    v0.17

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kurir_kopi/models/user.dart';
+import 'package:kurir_kopi/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -42,9 +43,16 @@ class AuthService {
   //register with email & password
   Future registerWithEmail(String email, String password) async {
     try {
+      //creates user data
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+
+      //create a new doc for the user with the uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'Pengguna Baru', 100);
+
+      //returns the user uid
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
